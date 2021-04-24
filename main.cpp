f53f22c8c95184f9b4aaa3cc4757d99e6e96ba34
miracle173 <>
2021-04-24 15:12:31 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,202 @@
+/*
+if a positive integer number n has a prime divisor p>=sqrt(n), then n=p*q and p and q are uniquely defined and (p,q)=1 and q<=sqrt(n).
+More general: if n is a positive integer number then there is a prime p such that n=(p^e)*q1*q2, such that (p,q1)=(p,q2)=(q1,q2)=1 and q1<=sqrt(n), q2<=sqrt(n). p,e,q1,q2 are not uniquely defined.
+this program calculates and stores such factors q1,q2,p^e, for every n between sqrt(limit) and limit
+*/
+#include <iostream>
+#include <cmath>
+#include <cassert> 
+#include <numeric>
+//#include <limits>
+
+const int limit=1000000;
+int sqrtlimit;
+
+int sieve[limit]={0};
+
+int smallfactor1[limit]={0};
+int primepowerfactor[limit]={0};
+int smallfactor2[limit]={0};
+
+void init_sieve(void){
+    /* 
+    initializes the array sieve
+    sieve[n]==n if n is a prime or n==1 or n==0, otherwise
+    sieve[n] is largest prime factor of n that 
+    is smaller than sqrtkimit
+    */
+    sqrtlimit=int(std::pow(limit,.5))+1;
+    for (int n=0;n<limit;n++){
+        sieve[n]=n;
+    }
+    for (int n=2;n<=sqrtlimit;n++){
+        if (sieve[n]==n){
+            for (int i=n;i<limit;i+=n){
+                sieve[i]=n;
+            }
+        }
+    }
+}
+
+void split_factors(void){
+    /* 
+    uses the initialized array sieve
+    sets the arrays smallfactor1, smalfactor2, primepowerfactor
+    splits a number in three, pairwise relativly prime factors
+    where the first and the third are smaller than sqrtlimit and the  second is a prime power
+    */
+    enum State {
+        continue_first_factor,
+        in_second_factor,
+        continue_third_factor
+        };
+    for (int n=sqrtlimit; n<limit; n++){
+        int current_prod=1;
+        int quotient=n;
+        int last_prime=0;
+        int prime_exponent=0;
+        int first_factor=1;
+        int second_factor=1;
+        int third_factor=1;
+        int prime_power=1;
+        int p;
+        State state=continue_first_factor;
+        while(sieve[quotient]>1){
+            p=sieve[quotient];
+            quotient/=p;
+            /*
+            if last prime power was found, 
+            put it to first factor
+            and restart calculating the next prime power
+            */
+            if (p!=last_prime){
+                switch (state) {
+                    case continue_first_factor:
+                        first_factor*=prime_power;
+                        break;
+                    case in_second_factor:
+                        second_factor=prime_power;
+                        current_prod=1;
+                        state=continue_third_factor;
+                        break;
+                    case continue_third_factor:
+                        third_factor*=prime_power;
+                        break;
+                    default:
+                        assert(0);
+                        break;
+                }
+                last_prime=p;
+                prime_power=p;
+            }
+            else {
+                prime_power*=p;
+            }
+            if (state==continue_first_factor){
+                current_prod*=p;
+                if (current_prod>sqrtlimit){
+                    state=in_second_factor;
+                }
+            }
+            /* process the last prime power */
+            if (quotient==1){
+                switch (state) {
+                    case continue_first_factor:
+                        first_factor*=prime_power;
+                        break;
+                    case in_second_factor:
+                        second_factor=prime_power;
+                        current_prod=1;
+                        state=continue_third_factor;
+                        break;
+                    case continue_third_factor:
+                        third_factor*=prime_power;
+                        break;
+                    default:
+                        assert(0);
+                        break;
+                }
+            }
+        }
+        smallfactor1[n]=first_factor;
+        primepowerfactor[n]=second_factor;
+        smallfactor2[n]=third_factor;
+    }
+}
+
+int powermod(int base, int exponent, int modulus){
+    long int prod=1;
+    long int base2=base;
+    int exponent2=exponent;
+    // invariant:
+    // (base2**exponent2)*prod==(base**exponent) (mod modulus)
+    while(exponent2>0){
+        if (exponent2%2==1){
+            prod*=base2;
+            prod%=modulus;
+            exponent2-=1;
+        }
+        else {
+            exponent2/=2;
+            int last_base2=base2;
+            base2*=base2;
+            base2%=modulus;
+        }
+    }
+    return prod;
+}
+
+bool is_prime(int n){
+    return sieve[n]==n;
+}
+
+int order(int element, int modulus){
+    while (not is_prime(n)){
+        
+    }
+}
+
+int main() {
+    int range = 30;
+    init_sieve();
+    split_factors();  
+    // some sample output
+    std::cout<<"limit "<<limit<<std::endl;
+    std::cout<<"sqrtlimit "<<sqrtlimit<<std::endl;
+    std::cout<<std::endl;
+    std::cout<<"max int "<<std::numeric_limits<int>::max()<<"\n";
+    std::cout<<"max long int "<<std::numeric_limits<long int>::max()<<"\n\n";
+    std::cout<<"print some prime factorization"<<std::endl;
+    for (int n=limit-range;n<limit; n++){
+        int m=n;
+        std::cout << m << " = " ;
+        bool first=true;
+        while (m>1){
+            if (first){
+                first=false;
+            }
+            else {
+                std::cout << " * ";
+            }
+            std::cout <<sieve[m];
+            m=m/sieve[m];
+        }
+        std::cout <<std::endl;
+    }
+    std::cout<<std::endl<<"smallfactor * primepower * smallfactor"<<std::endl;
+    std::cout<<"\tthe second factor is a prime power"<<std::endl;
+    std::cout<<"\t'!' at the end prime power exponent > 1"<<std::endl;
+
+    for (int n=limit-range;n<limit; n++){
+        std::cout<<n<<" = "<<smallfactor1[n]<<" * "<<primepowerfactor[n]<<" * "<<smallfactor2[n];
+        if (sieve[primepowerfactor[n]]!=primepowerfactor[n]){
+            std::cout<<" !";
+        }
+        std::cout<<std::endl;
+    }
+    for (int n=limit-range; n<limit; n++){
+        int x = 7;
+        int e=n-1;
+        std::cout<< x<<"^"<<e<<" = "<<powermod(x,e,n)<<" (mod "<<n<<")"<<"\n";
+     }
+}
